71aed62f806df130e107b45937abf6f2ed4edca0
Jack Wilkinson <me@jackwilky.com>
2026-02-11 14:17:16 +0000

    Removed unused import

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import yaml from '@rollup/plugin-yaml'
-import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
     build: {
